76e8e9b76184c54f55ecb00da5bfc392f5b5fb19
DESKTOP-SK33IGL\dimav <dimavr5@mail.ru>
2018-02-16 22:50:45 +0200

    Init

---
+++ frontend/views/site/login.php
@@ -0,0 +1,67 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap\ActiveForm */
+/* @var $model \common\models\LoginForm */
+
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+use dosamigos\tinymce\TinyMce;
+
+$this->title = 'Login';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-login">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>Please fill out the following fields to login:</p>
+
+    <div class="row">
+        <div class="col-lg-5">
+            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+
+                <?//= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+
+                <?= $form->field($model, 'password')->passwordInput() ?>
+
+                <?= $form->field($model, 'rememberMe')->checkbox() ?>
+
+                <div style="color:#999;margin:1em 0">
+                    If you forgot your password you can <?= Html::a('reset it', ['site/request-password-reset']) ?>.
+                </div>
+
+                <div class="form-group">
+                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                </div>
+
+            <?php ActiveForm::end(); ?>
+        </div>
+    </div>
+    <?
+    echo $form->field($model, 'username')->widget(TinyMce::className(), [
+        'options' => ['rows' => 15],
+        'language' => 'ru',
+        'clientOptions' => [
+            'plugins' => [
+                'advlist autolink lists link charmap  print hr preview pagebreak',
+                'searchreplace wordcount textcolor visualblocks visualchars code fullscreen nonbreaking',
+                'save insertdatetime media table contextmenu template paste image'
+            ],
+            'toolbar' => 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+           // "toolbar1"=> " undo redo preview code | link  bold italic underline | hr | image responsivefilemanager media |unlink anchor  | ",
+            'toolbar' => 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | responsivefilemanager link image media | forecolor backcolor | fontsizeselect',
+
+
+            'external_filemanager_path' => '/admin/plugins/responsivefilemanager/filemanager/',
+            'filemanager_title' => 'Responsive Filemanager',
+            'external_plugins' => [
+                //Иконка/кнопка загрузки файла в диалоге вставки изображения.
+                'filemanager' => '/admin/plugins/responsivefilemanager/filemanager/plugin.min.js',
+                //Иконка/кнопка загрузки файла в панеле иснструментов.
+                'responsivefilemanager' => '/admin/plugins/responsivefilemanager/tinymce/plugins/responsivefilemanager/plugin.min.js',
+            ],
+
+        ]
+    ]);
+    ?>
+</div>
