4ca41fb708df68c6c2b67b1161cb4aa9b835f1ca
novak-ol <alexleonnovak@gmail.com>
2018-04-05 13:00:27 +0300

    Вроде получилось исправить ложное срабатывание

--- frontend/modules/testusers/assets/common.js
+++ frontend/modules/testusers/assets/common.js
@@ -19,46 +19,24 @@ if (timer) {
             $('button[type="submit"]').val('0').submit();
         }
     }, 100);
+    var btn_click = false;
     $('button[type="submit"]').click(function(){
         $.post('total', {totaltime:(t-i)/10});
+        btn_click = true;
     });
     
-    $(window).mouseout(function(e){
-//        var params = window.location.search.replace('?','').split('&').reduce(
-//            function(p,e){
-//                var a = e.split('=');
-//                p[ decodeURIComponent(a[0])] = decodeURIComponent(a[1]);
-//                return p;
-//            },
-//        );
+    $(window).mouseleave(function(e){
         var strGET = window.location.search.replace( '?', ''); 
-//        var data = [];
-//        if (params['id_test']){
-//            data['id_test'] = params['id_test'];
-//        } else {
-//            data['id_theme'] = params['id_theme'];
-//        }
         var out = false;
-            if (e.toElement === null ) {
+        //console.log('click: '+btn_click);
+            if (e.toElement === null && btn_click === false) {
                 clearInterval(counterBack);
                 $.post('total?' + strGET, {out:true});
-                //console.log('the mouse left the window'); 
+//                console.log('the mouse left the window');
             }
     });
 }
-//window.onbeforeunload = function() {alert("пока");};
-//window.onbeforeunload = function() {
-//    return 'You have unsaved changes!';
-//};
-//function fullscreen(element) {
-//  if(element.requestFullScreen) {
-//    element.requestFullScreen();
-//  } else if(element.mozRequestFullScreen) {
-//    element.mozRequestFullScreen();
-//  } else if(element.webkitRequestFullScreen) {
-//    element.webkitRequestFullScreen();
-//  }
-//}
+
 var _href = $('#test-modal').find('.modal-footer a').attr('href');
 $('li a').click(function(){
     var id_test = $(this).attr('id_test');
